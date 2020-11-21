3020151315763832ff815e8ba46c2410bcf91ea6
jlachor <jlachor@exbet.io>
2020-11-21 11:13:44 +0100

    Added pic and carousel

--- src/Components/Pages/MainPage.jsx
+++ src/Components/Pages/MainPage.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Carousel } from 'antd';
 import pic1 from '../../Assets/pic1.jpg';
+import pic2 from '../../Assets/pic2.jpg';
+import pic3 from '../../Assets/pic3.jpg';
 import './MainPage.css';
 
 const MainPage = () => {
   return(
     <div className='main-page'>
+      <div className='carusel-placeholder'>
+        <Carousel autoplay={true}>
+          <img src={pic1} alt='Pic1' className='standard-pic'/>
+          <img src={pic2} alt='Pic1' className='standard-pic'/>
+          <img src={pic3} alt='Pic1' className='standard-pic'/>
+        </Carousel>
+      </div>
       <div className='section most-important'>
         <div className='special'>
-          <img src={pic1} alt='Pic1' className='standard-pic'/>
+
         </div>
         <div className='text'>
           <p>
