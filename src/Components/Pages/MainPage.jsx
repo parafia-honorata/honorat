321beaf1496ffc1f5fd0e20ccbe3a0dfb1c65541
jlachor <jlachor@exbet.io>
2020-12-05 10:44:11 +0100

    Added a lot of new content to main page

--- src/Components/Pages/MainPage.jsx
+++ src/Components/Pages/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Carousel, Image } from 'antd';
+import { Carousel, Image, Button } from 'antd';
 import pic1 from '../../Assets/pic1.jpg';
 import pic2 from '../../Assets/pic2.jpg';
 import pic3 from '../../Assets/pic3.jpg';
 import pic5 from '../../Assets/pic5.jpg';
 import pic7 from '../../Assets/pic7.jpg';
+import pic8 from '../../Assets/pic8.jpg';
 import './MainPage.css';
 
 const MainPage = () => {
@@ -51,11 +52,15 @@ const MainPage = () => {
           <Image src={pic5} preview={false} alt='pic5' className='vertical-image' />
         </div>
         <div className='text'>
+          <h2>O parafii</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Parafia pw. błogosławionego Honorata Koźmińskiego została powołana stosunkowo niedawno i jest najmłodszą
+            parafią w Będzinie-Grodźcu. Założono ją w celu posługi duszpasterskiej dla mieszkańców kopalnianego osiedla
+            przy ulicy Konopnickiej. 
+          </p>
+          <p>
+            Choć ustanowienia parafii dokonał biskup częstochowski Stanisław Nowak, obecnie należy ona do Diecezji Sosnowieckiej.
+            Biskup Adam Śmigielski dokonał jej uroczystej konsekracji 15 października 2000 r.
           </p>
         </div>
       </div>
@@ -64,15 +69,33 @@ const MainPage = () => {
           <Image src={pic7} preview={false} alt='Pic7' className='vertical-image' />
         </div>
         <div className='text'>
-          
+          <h2>Błogosławiony Honorat Koźmiński</h2>
+          <p>
+            Życie błogosławionego Honorata pokazuje, że Pan Bóg ma dla każdego z nas plan, oraz że drogę do świętości poprzedza
+            czasem zwątpienie i kryzys wiary. Buntownik z pobożnej inteligenckiej rodziny stał się jedną z najważniejszych postaci
+            życia religijnego polaków pod zaborami.
+          </p>
+          <p>
+            W młodości, idąc za przykładem ojca, uczył się na architekta. Wstąpił do płockiego gimnazjum. Zostaje doświadczony
+            na wiele sposobów, młodzieńczym niedowiarstwem, przedwczesną śmiercą ojca, a wreszcie wtrąceniem do carskiego więzienia.
+            Z warszawskiej Cytadeli wyszedł odmieniony. Po przeżytym nawróceniu stał się spokojny i pogodzony z Bogiem. Niedługo później
+            rozpoczął swoje życie zakonne jako Kapucyn.
+          </p>
+          <p>
+            W trudnych warunkach zaborów, nieustannie podejrzewany przez władzę rosyjskie o działalność wywrotową i konspirację, założył
+            23 zgromadzenia zakonne i 26 stowarzyszeń tercjarskich. Jednocześnie też promował ukryte zgromadzenia zakonne, jako formę
+            oporu wobec rosyjskiej opresji. Był uznanym spowiednikiem, tytanem pracy i niestrudzonym organizatorem. Zmarł w 1916 r. w 
+            opinii świętości.
+          </p>
+          <Button type="primary">Więcej</Button>
         </div>
       </div>
       <div className='section map'>
         <div className='special'>
-
+          <Image src={pic8} preview={false} alt='pic8' className='vertical-image' />
         </div>
         <div className='text'>
-          
+          CO TO JEST?
         </div>
       </div>
     </div>
