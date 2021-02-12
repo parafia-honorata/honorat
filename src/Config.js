28a70781f51d53ca0c8698d99586d1b5b52f9e2c
jlachor <jlachor@exbet.io>
2021-02-12 10:01:26 +0100

    New config and logo

--- src/Config.js
+++ src/Config.js
@@ -4,7 +4,8 @@ const Config = {
     apiKey: 'AIzaSyC2HcEha4-9jTngjDxFMSYJhgVZjilIltE',
     newsPostKey: '',
     intensionsPostKey: ''
-  }
+  },
+  josephYearLink: 'https://www.vaticannews.va/pl/papiez/news/2020-12/papiez-oglasza-rok-poswiecony-swietemu-jozefowi.html'
 }
 
 export default Config;
