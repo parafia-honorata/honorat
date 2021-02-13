556c3dd2c77987b6dc5d010486d4b163651b606a
jlachor <jlachor@exbet.io>
2021-02-13 09:49:28 +0100

    Prepared new navbar for mobile

--- src/Components/FixedMenu.jsx
+++ src/Components/FixedMenu.jsx
@@ -1,19 +1,29 @@
-import { Button } from 'antd';
+import { Button, Drawer } from 'antd';
 import React, {useEffect, useState} from 'react';
 import './FixedMenu.css';
 import temporaryLogo from '../Assets/LogoChurch.png';
 import {Link, useLocation} from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive'
+import {MenuOutlined} from '@ant-design/icons';
 
 const FixedMenu = () => {
 
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   const [menuKey, setMenuKey] = useState('main');
+  const [drawerVisible, setDrawerVisible] = useState(false);
   let location = useLocation();
 
   useEffect(() => {
     setMenuKey(location.pathname);
   }, [location])
 
+  const onOpen = () => {
+    setDrawerVisible(true);
+  }
 
+  const onClose = () => {
+    setDrawerVisible(false);
+  }
 
   const handleClick = (e) => {
    setMenuKey(e.key);
@@ -47,9 +57,24 @@ const FixedMenu = () => {
     <React.Fragment>
       <Link className='pic-link' to='/'><img src={temporaryLogo} alt='logo' className='logo'/></Link>
       <div className='fixed-menu'>
-        <div className='menu-list'>
-          {itemList.map((item) => <Link to={item.key}><Button type="primary" id={item.key} onClick={handleClick}>{item.title}</Button></Link>)}
-        </div>
+        {isMobile ? 
+          <div className='menu-list'>
+            <MenuOutlined onClick={onOpen} />
+            <Drawer
+              title="Menu"
+              placement='right'
+              onClose={onClose}
+              visible={drawerVisible}
+            >
+              {itemList.map((item) => <Link to={item.key}><Button type="primary" id={item.key} onClick={handleClick}>{item.title}</Button></Link>)}
+            </Drawer>
+          </div>
+          :
+          <div className='menu-list'>
+            {itemList.map((item) => <Link to={item.key}><Button type="primary" id={item.key} onClick={handleClick}>{item.title}</Button></Link>)}
+          </div>
+        }
+
       </div>
     </React.Fragment>
   )
