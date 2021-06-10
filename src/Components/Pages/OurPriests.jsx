a8f81bc0d46e9f7a702a31fed176765ac683ff2d
jlachor <jlachor@exbet.io>
2021-06-10 09:46:53 +0200

    Changed logo, resize OurPriests page

--- src/Components/Pages/OurPriests.jsx
+++ src/Components/Pages/OurPriests.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react';
 import './OurPriests.css';
 import {Image} from 'antd';
+import { useMediaQuery } from 'react-responsive'
 import priestpic1 from '../../Assets/priestpic1.jpg';
 import priestpic2 from '../../Assets/priestpic2.jpg';
 import pic21 from '../../Assets/pic21.jpg';
 
 const OurPriests = () => {
 
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -14,29 +16,42 @@ const OurPriests = () => {
   return(
     <div className='our-priests sub-page'>
       <h1>Nasi Księża</h1>
-      <div className='section card ks-marek'>
-        <div className='special'>
-          <Image src={priestpic1} preview={false} alt='pic' className='vertical-image' />
-        </div>
-        <div className='text'>
-            <h2>Ksiądz Administator Marek Zaręba</h2>
-            <h4>O sobie:</h4>
-            <p>
-              Święcenia kapłańskie przyjąłem w 1991r w Częstochowie. Od święceń do dziś pełniłem posługę duszpasterską kolejno w Polsce, Zambii, 
-              ponownie w Polsce, krótki czas w Austrii i aktualnie w par. pw. bł. Honorata Koźmińskiego w Będzinie-Grodźcu.
-            </p>
-            <h3>Tel: 690-649-750</h3>
-        </div>
-      </div>
-      <div className='section card ks-janusz reverted'>
-        <div className='special'>
-          <Image src={priestpic2} preview={false} alt='pic' className='vertical-image' />
-        </div>
-        <div className='text'>
-          <h2>Ksiądz Wikariusz Janusz Pieśniewski</h2>
-          <h3>Tel: 503-605-330</h3>
-        </div>
-      </div>
+      { isMobile ? 
+        <React.Fragment>
+          <div className='section card ks-marek'>
+            <div className='special'>
+              <Image src={priestpic1} preview={false} alt='pic' className='vertical-image' />
+            </div>
+            <div className='text'>
+              <h2>Ksiądz Administator Marek Zaręba</h2>
+              <h3>Tel: 690-649-750</h3>
+            </div>
+          </div>
+          <div className='section card ks-janusz reverted'>
+            <div className='special'>
+              <Image src={priestpic2} preview={false} alt='pic' className='vertical-image' />
+            </div>
+            <div className='text'>
+              <h2>Ksiądz Wikariusz Janusz Pieśniewski</h2>
+              <h3>Tel: 503-605-330</h3>
+            </div>
+          </div>
+        </React.Fragment>
+          :
+          <div className='section card priests'>
+            <div>
+              <Image src={priestpic1} preview={false} alt='pic' className='vertical-image' />
+              <h2>Ksiądz Administator Marek Zaręba</h2>
+              <h3>Tel: 690-649-750</h3>
+            </div>
+            <div>
+              <Image src={priestpic2} preview={false} alt='pic' className='vertical-image' />
+              <h2>Ksiądz Wikariusz Janusz Pieśniewski</h2>
+              <h3>Tel: 503-605-330</h3>
+            </div>
+          </div>
+      }
+
       <div className='section card ks-past'>
         <div className='special'>
           <Image src={pic21} preview={false} alt='pic' className='vertical-image' />
