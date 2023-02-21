dc29f6270ad3ded6a4fa4ae353c98c7795bf2afa
jlachor <jlachor@exbet.io>
2023-02-21 17:30:05 +0100

    Resize images

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 import './About.css';
 import {Image} from 'antd';
-import pic22 from '../../Assets/pic22.jpg';
 import pic21 from '../../Assets/pic21.jpg';
 
 const About = () => {
@@ -38,27 +37,6 @@ const About = () => {
               Po prawie dzięsięciu latach udało się zakończyć budowę. Kościół ma plan sześciokąta foremnego i może pomieścić
               około 500 osób. Szybka i sprawna budowa udała się dzięki wspaniałej ofiarności parafian. W 2000 roku nastąpiła
               uroczysta konsekracja świątynii.
-            </p>
-          </div>
-        </div>
-        <div className='section card organisation'>
-          <div className='special'>
-            <Image src={pic22} preview={false} alt='pic' className='vertical-image' />
-          </div>
-          <div className='text'>
-            <h2>Rok Kościelny i Duszpasterski 2021/2022</h2>
-            <h2>„Posłani w pokoju Chrystusa”</h2>
-            <p>
-              <i>Ten kto Mnie spożywa, będzie żył przeze Mnie"</i> (J 6,57)
-            </p>
-            <p>
-              W roku 2022 realizujemy w Polsce kontynuacje Programu Duszpasterskiego „Eucharystia daje życie”. Kościół zachęca nas
-              do dostrzeżena nadzwyczajnej roli Eucharystii w naszym życiu oraz rzeczywistej obecności Pana Jezusa w Najświętszym Sakramencie.
-            </p>
-            <p>
-              Jak powiedział św. Jan Paweł II: „W Najświętszej Eucharystii zawiera się bowiem całe dobro duchowe Kościoła, to znaczy sam Chrystus, 
-              nasza Pascha i Chleb żywy, który przez swoje ożywione przez Ducha Świętego i ożywiające Ciało daje życie ludziom. Dlatego też 
-              Kościół nieustannie zwraca swe spojrzenie ku swojemu Panu, obecnemu w Sakramencie Ołtarza, w którym objawia On w pełni ogrom swej miłości”.
             </p>
           </div>
         </div>
