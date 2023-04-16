3b57cc7a56f272d78106e07a94eace6ff219be2f
jlachor <jlachor@exbet.io>
2023-04-16 16:58:50 +0200

    remove special Easter section

--- src/Components/Pages/News.jsx
+++ src/Components/Pages/News.jsx
@@ -2,8 +2,6 @@ import React, {useEffect, useState} from 'react';
 import './News.css';
 import BlogService from '../../BlogService';
 import parse from 'html-react-parser';
-import { Button } from 'antd';
-import {Link} from 'react-router-dom';
 
 const News = () => {
 
@@ -22,16 +20,10 @@ const News = () => {
   return(
     <div className='news sub-page'>
       <div className='card blog'>
-        <div className="above-news">
-          <h2>Plan Wielkiego Tygodnia:</h2>
-          <Link to={'/dodatkowe-informacje'}>
-            <Button type="primary">Wielki Tydzień</Button>
-          </Link>
-        </div>
-      </div>
-      <div className='card blog'>
-        {blogPost && blogPost.content &&
+        {(blogPost && blogPost.content) ?
           parse(blogPost.content)
+          :
+          <div class="lds-dual-ring"></div>
         }
       </div>
     </div>
